ae4d4207e5be189d9b1627be4cee272cb75c0b6d
Ritvik <1115575@apps.nsd.org>
2025-01-14 10:59:53 -0800

    finished hailstone

--- Efficiency Warmups/efficiency/hailstone.py
+++ Efficiency Warmups/efficiency/hailstone.py
@@ -1,7 +1,8 @@
+import builtins; builtins.profile = builtins.__dict__.get('profile', lambda x: x)
 from cse163_utils import assert_equals
 from time_graph import TimeGraph
 
-
+@profile
 def my_hailstone_seq(n):
     """
     See instructions for more information.
@@ -16,14 +17,14 @@ def my_hailstone_seq(n):
     ans = dict()
     for num in range(1, n + 1):
         nextnum = num
-        appender = {num}
+        steps = 1
         while nextnum != 1:
             if nextnum % 2 == 0:
                 nextnum = nextnum / 2
             else:
                 nextnum = nextnum * 3 + 1
-            appender.add(nextnum)
-        ans[num] = len(appender)
+            steps = steps + 1
+        ans[num] = steps
     return ans
 
 
@@ -31,38 +32,37 @@ def dynamic_hailstone_seq(n):
     # Your implementation goes here
     ans = dict()
     for num in range(1, n + 1):
-        solution = dict()
         nextnum = num
-        appender = {num}
-        while n != 1:
+        steps = 1
+        while nextnum != 1:
+            if nextnum in ans:
+                steps += ans[nextnum] - 1
+                break
             if nextnum % 2 == 0:
                 nextnum = nextnum / 2
             else:
                 nextnum = nextnum * 3 + 1
-            if nextnum in solution:
-                solution[n] = 1 + solution[nextnum]
-                continue
-            n = nextnum
-            appender.add(nextnum)
-        ans[num] = len(appender)
+            steps += 1
+        ans[num] = steps
     return ans
 
 
 # Test my_get_primes
 def test_hailstone():
-    fiddy = {1:1, 2:2, 3:8, 4:3, 5:6, 6:9, 7:17, 8:4, 9:20, 10:7, 11:15, 12:10, 13:10, 14:18, 15:18, 16:5}
+    fiddy = {1: 1, 2: 2, 3: 8, 4: 3, 5: 6, 6: 9, 7: 17, 8: 4, 9: 20, 10: 7, 11: 15, 12: 10, 13: 10,
+             14: 18, 15: 18, 16: 5}
     assert_equals(fiddy, dynamic_hailstone_seq(16))
     print("set_of_hailstone Success")
 
 
 def graph_prime_performance():
     timer = TimeGraph()
-    timer.time_and_graph(my_hailstone_seq)
+    timer.time_and_graph(dynamic_hailstone_seq)
 
 
 def main():
     test_hailstone()
-    # graph_prime_performance()
+    graph_prime_performance()
     # print(webs_get_primes(50))
 
 
